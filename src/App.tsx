78a6154b2887dbd861facf95a9f5a9ae77626a0a
Arpit Kaushik <arpitkaushik@Arpits-Mac-mini.local>
2023-12-08 20:36:38 +0530

    code cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import LandingPage from './screens/landingPage';
+import LandingPage from './screens/landingScreen';
 
 function App() {
   return (
--- src/screens/landingScreen/index.tsx
+++ src/screens/landingScreen/index.tsx
@@ -1,22 +1,22 @@
 import { useEffect, useState } from "react";
-import NumbersWithRhymes from './constants/numberWithRhymes.json';
+import NumbersWithRhymes from '../constants/numberWithRhymes.json';
 
 const LandingPage = () => {
 
     const tambolalength = new Array(90);
 
     const [tambolaNumbers, setTambolaNumbers]: any = useState([]);
-    const [totalNumbersLeft, settotalNumbersLeft] = useState([]);
+    const [totalNumbersLeft, setTotalNumbersLeft] = useState([]);
 
     useEffect(() => {
         const nums: any = [];
         const totalNums: any = [];
         if (totalNumbersLeft.length === 0 && tambolaNumbers.length === 0) {
-            tambolalength.fill('').map((i, index: any) => {
+            tambolalength.fill('').forEach((i, index: any) => {
                 totalNums.push(index + 1);
                 nums.push({ number: index + 1, active: false });
             })
-            settotalNumbersLeft(totalNums);
+            setTotalNumbersLeft(totalNums);
             setTambolaNumbers(nums);
         }
     }, [tambolaNumbers])
@@ -27,13 +27,11 @@ const LandingPage = () => {
         const newNumberIndex = totalNumbersLeft.indexOf(newNumber);
         totalNumbersLeft.splice(newNumberIndex, 1);
         speechSynthesis.speak(new SpeechSynthesisUtterance(` ${newNumber} ${NumbersWithRhymes[newNumber]}`));
-
         updateNewNumberOnBoard(newNumber);
 
     }
 
     const updateNewNumberOnBoard = (newNumber: any) => {
-        // const tambolaNums: any = tambolaNumbers;
         for (let i = 0; i < tambolaNumbers.length; i++) {
             if (tambolaNumbers[i].number === newNumber) {
                 tambolaNumbers[i].active = true;
@@ -51,9 +49,8 @@ const LandingPage = () => {
             <header className="header">Welcome to TVB (Tambola Virtual Board)</header>
             <div className="grid-container">
                 {tambolaNumbers.map((item: any, index:any) => {
-                    console.log(item.active)
                     return (
-                        <div className={`grid-item ${item.active ? `active-cell` : ``}`}>{item.number}</div>
+                        <div key={item.number.toString()} className={`grid-item ${item.active ? `active-cell` : ``}`}>{item.number}</div>
                     )
                 })}
             </div>
