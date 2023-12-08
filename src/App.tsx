64b1617c08cd381c4e7ebd2f6431010271ebf8a2
Arpit Kaushik <arpitkaushik@Arpits-Mac-mini.local>
2023-12-08 20:26:14 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LandingPage from './screens/landingPage';
+
+function App() {
+  return (
+    <div className="App">
+     <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/screens/landingPage.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import NumbersWithRhymes from './constants/numberWithRhymes.json';
+
+const LandingPage = () => {
+
+    const tambolalength = new Array(90);
+
+    const [tambolaNumbers, setTambolaNumbers]: any = useState([]);
+    const [totalNumbersLeft, settotalNumbersLeft] = useState([]);
+
+    useEffect(() => {
+        const nums: any = [];
+        const totalNums: any = [];
+        if (totalNumbersLeft.length === 0 && tambolaNumbers.length === 0) {
+            tambolalength.fill('').map((i, index: any) => {
+                totalNums.push(index + 1);
+                nums.push({ number: index + 1, active: false });
+            })
+            settotalNumbersLeft(totalNums);
+            setTambolaNumbers(nums);
+        }
+    }, [tambolaNumbers])
+
+    const onShuffle = (): any => {
+
+        const newNumber = getRandom();
+        const newNumberIndex = totalNumbersLeft.indexOf(newNumber);
+        totalNumbersLeft.splice(newNumberIndex, 1);
+        speechSynthesis.speak(new SpeechSynthesisUtterance(` ${newNumber} ${NumbersWithRhymes[newNumber]}`));
+
+        updateNewNumberOnBoard(newNumber);
+
+    }
+
+    const updateNewNumberOnBoard = (newNumber: any) => {
+        // const tambolaNums: any = tambolaNumbers;
+        for (let i = 0; i < tambolaNumbers.length; i++) {
+            if (tambolaNumbers[i].number === newNumber) {
+                tambolaNumbers[i].active = true;
+            }
+        }
+        setTambolaNumbers([...tambolaNumbers]);
+    }
+
+    const getRandom = () => {
+        return totalNumbersLeft[Math.floor((Math.random() * totalNumbersLeft.length))];
+    }
+
+    return (
+        <div>
+            <header className="header">Welcome to TVB (Tambola Virtual Board)</header>
+            <div className="grid-container">
+                {tambolaNumbers.map((item: any, index:any) => {
+                    console.log(item.active)
+                    return (
+                        <div className={`grid-item ${item.active ? `active-cell` : ``}`}>{item.number}</div>
+                    )
+                })}
+            </div>
+
+            <button onClick={() => onShuffle()} className="button-shuffle">
+                Shuffle
+            </button>
+        </div>
+    )
+}
+
+export default LandingPage;
